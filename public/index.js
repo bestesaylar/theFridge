b0bcd5119d0b5b07a5da4f6864b0ff5d92bb91d4
Glitch (hello-express) <none>
2020-11-17 23:55:03 +0000

    👠🛫 Checkpoint ./server.js:7456/113 ./public/index.js:7456/316

--- public/index.js
+++ public/index.js
@@ -2,42 +2,47 @@ let canvas = document.getElementById("canvas");
 let upload = document.getElementById("upload");
 let posts = document.getElementById("posts");
 
-function getPosts() {
-  fetch("/posts", {
-    method: "get"
-  })
-    .then(res => res.json())
-    .then(response => {
-      console.log("posts:", JSON.stringify(response));
+// fetch posts from server
+// function getPosts() {
+//   fetch("/posts", {
+//     method: "get"
+//   })
+//     .then(res => res.json())
+//     .then(response => {
+//       console.log("posts:", JSON.stringify(response));
 
-      let images_html = response
-        .map(file_url => {
-          return `<img src="uploaded/${file_url}">`;
-        })
-        .join("\n");
-      posts.innerHTML = images_html;
-    });
-}
-getPosts();
+//       let images_html = response
+//         .map(file_url => {
+//           return `<img src="uploaded/${file_url}">`;
+//         })
+//         .join("\n");
+//       posts.innerHTML = images_html;
+//     });
+// }
+// getPosts();
 
-upload.addEventListener("click", e => {
-  let payload = {
-    image: canvas.toDataURL("image/png"),
-    name: "my name"
-  };
-  fetch("/upload", {
-    method: "POST",
-    body: JSON.stringify(payload), // data can be `string` or {object}!
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-    .then(res => res.json())
-    .then(response => {
-      console.log("Success:", JSON.stringify(response));
-      getPosts();
-    });
-});
+//UPLOAD CANVAS TO SERVER
+// upload.addEventListener("click", e => {
+//   let payload = {
+//     image: canvas.toDataURL("image/png"),
+//   };
+//   fetch("/upload", {
+//     method: "POST",
+//     body: JSON.stringify(payload), // data can be `string` or {object}!
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   })
+//     .then(res => res.json())
+//     .then(response => {
+//       console.log("Success:", JSON.stringify(response));
+//       getPosts();
+//     });
+// });
+
+
+
+// DRAWING STUFF
 var ctx = canvas.getContext("2d");
 
 let mousedown = false;
@@ -60,18 +65,22 @@ canvas.addEventListener("mousemove", e => {
     return;
   }
   let bounds = canvas.getBoundingClientRect();
+  ctx.strokeStyle = "black";
 
   ctx.beginPath();
   ctx.moveTo(last_x, last_y);
   ctx.lineTo(e.clientX - bounds.left, e.clientY - bounds.top);
   ctx.stroke();
   ctx.translate(3, 3);
+  ctx.strokeStyle = "#FF0000";
   ctx.beginPath();
   ctx.moveTo(last_x, last_y);
   ctx.lineTo(e.clientX - bounds.left, e.clientY - bounds.top);
   ctx.stroke();
   ctx.resetTransform();
   ctx.translate(-3, -3);
+    ctx.strokeStyle = "#0000FF";
+
   ctx.beginPath();
   ctx.moveTo(last_x, last_y);
   ctx.lineTo(e.clientX - bounds.left, e.clientY - bounds.top);
--- server.js
+++ server.js
@@ -5,36 +5,33 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const upload_folder = "tmp";
-
 app.use(express.static("public"));
-app.use(bodyParser.json());
-app.use("/uploaded", express.static(upload_folder));
 
-// get list of posts
-app.get("/posts", (req, res) => {
-  fs.promises.readdir(upload_folder).then(files => {
-    res.send(JSON.stringify(files));
-  });
-});
+// app.use(bodyParser.json());
+// app.use("/uploaded", express.static(upload_folder));
+
+// // get list of posts
+// app.get("/posts", (req, res) => {
+//   fs.promises.readdir(upload_folder).then(files => {
+//     res.send(JSON.stringify(files));
+//   });
+// });
 
 // Upload post route
-app.post("/upload", (req, res) => {
-  const { title, image } = req.body;
+// app.post("/upload", (req, res) => {
+//   const { title, image } = req.body;
 
-  let id = Math.random()
-    .toFixed(8)
-    .toString()
-    .slice(2);
-  var base64Data = image.replace(/^data:image\/png;base64,/, "");
+//   let id = Math.random()
+//     .toFixed(8)
+//     .toString()
+//     .slice(2);
+  
+//   var base64Data = image.replace(/^data:image\/png;base64,/, "");
 
-  fs.writeFile(`${upload_folder}/${id}.png`, base64Data, "base64", function(
-    err
-  ) {
-    console.log(err);
-  });
-  res.json({ id: id });
-  console.log("saved " + id);
-});
+//   fs.writeFile(`${upload_folder}/${id}.png`, base64Data, "base64");
+//   res.json({ id: id });
+//   console.log("saved " + id);
+// });
 
 // Server listener
 app.listen(process.env.PORT);

610c522b092e9eafc544f9da84cc728abd61bf1c
Glitch (hello-express) <none>
2020-11-17 22:46:04 +0000

    🚠⛸ Checkpoint ./public/index.js:7456/589

--- public/index.js
+++ public/index.js
@@ -1,6 +1,29 @@
 let canvas = document.getElementById("canvas");
 let upload = document.getElementById("upload");
 
+
+upload.addEventListener("click",e=>{
+  
+  let payload = {
+    img: canvas.toDataURL("image/png"),
+    name: "my name",
+
+  }
+  fetch(functions._url("upload"), {
+      method: "POST",
+      body: JSON.stringify(payload), // data can be `string` or {object}!
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(response => {
+        console.log("Success:", JSON.stringify(response));
+        this.play();
+      })
+  
+  
+})
 var ctx = canvas.getContext("2d");
 
 let mousedown = false;
